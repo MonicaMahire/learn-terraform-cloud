c1a85bb4aafcd84ca277162c3fa6ae2aa5a32795
MonicaMahire <152792711+MonicaMahire@users.noreply.github.com>
2023-12-10 20:25:25 +0530

    Create terraform.tf
    vcs driven workspace

---
+++ terraform.tf
@@ -0,0 +1,16 @@
+terraform {
+/*
+  cloud {
+   organization = "organization-name"
+
+   workspaces {
+     name = "learn-terraform-cloud"
+   }
+  }
+*/
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.28.0"
+    }
